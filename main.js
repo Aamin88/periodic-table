a8f362ea9b48ecda39491f3db2dff5dc307ccbb9
Aamin88 <forkahamin@yahoo.co.uk>
2024-08-10 08:04:54 +0100

    add: adding a tutor to show users how the website works, when its launch for the first time

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const driver = window.driver.js.driver;
+const driverObj = driver();
+
 const tdEls = document.querySelectorAll("td");
 const audioEls = document.querySelectorAll("audio");
 
-// const driverObj = driver();
+function tour() {
+  driverObj.highlight({
+    element: tdEls[0],
+    popover: {
+      description: "click any box to hear the name of the element",
+    },
+  });
+}
 
-// driverObj.highlight({
-//   element: tdEls[0],
-//   popover: {
-//     title: "Title",
-//     description: "Description",
-//   },
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const checkVisit = JSON.parse(localStorage.getItem("visit"));
+
+  console.log(checkVisit);
+
+  if (!checkVisit) {
+    tour();
+    localStorage.setItem("visit", JSON.stringify(true));
+  }
+});
 
 tdEls.forEach((tdEl) => {
   tdEl.addEventListener("click", (e) => callout(e, tdEl));
